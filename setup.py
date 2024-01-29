c19fd66f5e087b35d71fc31c55a8f58cf05ef86f
guillaumeesilv <150175977+guillaumeesilv@users.noreply.github.com>
2024-01-29 13:47:53 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='{name-of-your-project}',
+version='1.0',
+author='{your-full-name-or-your-github-username}',
+license='MIT',
+long_description=open('README.md').read(),
+)
