c3d67fa938d568cde40dc6a7eab3c8b7cb1678c7
guillaumeesilv <150175977+guillaumeesilv@users.noreply.github.com>
2024-01-29 14:24:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(
-name='{name-of-your-project}',
+name='    ASCII_Art_Generator-Awoukou_Guillaume-CDOF4',
 version='1.0',
-author='{your-full-name-or-your-github-username}',
+author='guillaumeesilv',
 license='MIT',
 long_description=open('README.md').read(),
 )
